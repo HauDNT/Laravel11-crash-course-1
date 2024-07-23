902f91b15e6d9396bf49a175db29f7da0b9034d7
HauDNT <tienhau.it@gmail.com>
2024-07-24 01:18:45 +0700

    Using middleware

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -6,6 +6,13 @@ use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
+    public function __construct()
+    {
+        // Áp dụng middleware 'auth' cho tất cả các phương thức trong controller
+        $this->middleware('auth');
+    }
+
+
     public function index() {
         return view("users.dashboard");
     }
